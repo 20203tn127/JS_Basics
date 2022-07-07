7dd7d3ff31759bea93cace742d8a2f0626aaa207
Angelinho Diaz <20203tn127@utez.edu.mx>
2022-07-06 20:34:56 -0500

    Leccion 07 - 2 functions

--- Leccion 07/07-01-functions.js
+++ Leccion 07/07-01-functions.js
@@ -4,6 +4,7 @@
 miFuncion(10,10)
 //Declaracion de una funcion
 function miFuncion(a,b){
+    console.log(arguments.length)
     console.log("Suma " + (a+b));
 }
 
@@ -21,9 +22,26 @@ console.log(resultado); //Se imprime el valor del return
 //Funciones tipo anonimas o tipo expresion
 //Declaracion de funcion anonima
 let suma = function(a,b){return (a + b)};
-
 resultado = suma(1,2);
 console.log(resultado);
 
+//funciones de tipo self - invoking
+//se define entre parentesis
+//Solamente se ejecuta una vez, ya no puede ser llamada
+(function(a,b){
+    console.log("Ejecutando una funcion self invoke: " + a + b)
+}) (5,7);
 
+//Funciones como objetos
+//Las funciones a su vez se pueden describir como objetos
+//Tienen propiedades y metodos
+let miFuncionText = miFuncion.toString();
+console.log(miFuncionText)
+
+//Arrow functions ()=>
+//Se introdujo en ECMAS6
+//Declaracion:
+//constante nombre         argumentos - cuerpo de la funcion
+const sumarArrowFunction = (a, b)     =>    a+b;
+console.log(sumarArrowFunction(3,5));
 
---
+++ Leccion 07/07-02-Args&Params.js
@@ -0,0 +1,3 @@
+//ARGUMENTOS Y PARAMETROS DE FUNCIONES
+//Los parametros son las variables que recibira una funcion
+//Los argumentos son los valores reales, digamos en crudo, ya sean numeros o string
